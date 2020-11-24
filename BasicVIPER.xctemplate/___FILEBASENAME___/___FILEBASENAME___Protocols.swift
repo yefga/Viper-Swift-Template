86c5a1ee30b9e4665baf7c82fa8b2b700cb0f468
Yefga <yefga.tp@icloud.com>
2020-11-24 21:26:53 +0700

    first commit

---
+++ BasicVIPER.xctemplate/___FILEBASENAME___/___FILEBASENAME___Protocols.swift
@@ -0,0 +1,73 @@
+/// Copyright © ___YEAR___. ___PROJECTNAME___. All rights reserved.
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+///
+///  ___FILENAME___
+///  ___PROJECTNAME___
+///
+///  Created by ___FULLUSERNAME___ on ___DATE___.
+///  Using Swift ___DEFAULTTOOLCHAINSWIFTVERSION___
+///  Running on macOS ___RUNNINGMACOSVERSION___
+///
+
+import Foundation
+
+protocol ___VARIABLE_productName___ViewToPresenterProtocol: class {
+    var view: ___VARIABLE_productName___PresenterToViewProtocol?{ get set }
+    var interactor: ___VARIABLE_productName___PresenterToInteractorProtocol? {get set}
+    var router: ___VARIABLE_productName___PresenterToRouterProtocol? {get set}
+
+}
+
+//MARK: Presenter -
+protocol ___VARIABLE_productName___PresenterToViewProtocol: class {
+    
+    func noticeShowLoading()
+   
+}
+
+//MARK: Interactor -
+protocol ___VARIABLE_productName___PresenterToInteractorProtocol: class {
+    
+    var presenter: ___VARIABLE_productName___InteractorToPresenterProtocol?  { get set }
+    func fetch___VARIABLE_productName___(result: String)
+   
+}
+
+//MARK: View -
+protocol ___VARIABLE_productName___InteractorToPresenterProtocol: class {
+    
+    func noticeShowLoading()
+  
+}
+
+//MARK: Router -
+protocol ___VARIABLE_productName___PresenterToRouterProtocol: class {
+    func createModule()-> ___VARIABLE_productName___ViewController
+    func goTo(viewController: ___VARIABLE_productName___ViewController)
+  
+}
