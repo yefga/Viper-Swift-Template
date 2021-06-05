d0613384260f0ce0a74cb497120441d139adb624
yefga <40693306+yefga@users.noreply.github.com>
2021-06-05 18:30:30 +0700

    adding commented deinit to retain cycle interactor

--- BasicVIPER.xctemplate/___FILEBASENAME___/___FILEBASENAME___Presenter.swift
+++ BasicVIPER.xctemplate/___FILEBASENAME___/___FILEBASENAME___Presenter.swift
@@ -44,6 +44,10 @@ class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_productName___ViewToPresenterP
     
     var router: ___VARIABLE_productName___PresenterToRouterProtocol?
     
+//     deinit {
+//         self.interactor = nil
+//     }
+    
 }
 
 extension ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_productName___InteractorToPresenterProtocol {
